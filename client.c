56d668c2180d646667b60ba4dbe44c577c208e12
Vartanyan Nadezhda <vart.n.25.05@gmail.com>
2013-11-20 20:03:16 +0400

    home task to November, 23

---
+++ client.c
@@ -0,0 +1,337 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define FILE_IN "./source.txt"
+#define FILE_LOG "./log.txt"
+#define IPC_ID_MSG 1
+#define IPC_ID_SHM 2
+#define IPC_ID_SEM 3
+#define MSG_MAX 4068 
+
+struct mult
+{
+	int str1;
+	int stlb2;
+	int wh;         //width or heigh
+	int *p1, *p2;   //pointers to the matrices
+	int *res;       //result of calculation
+};
+
+
+struct msgbuf_str
+{
+	long mtype;
+	char msg[MSG_MAX];
+};
+
+
+struct msgbuf_matr
+{
+	long mtype;
+	struct
+	{
+		int width;
+		int height1;
+		int height2;
+		int matrices[MSG_MAX];
+	} info;
+};
+
+
+void *multiplication(void *num0);
+int char_to_int(unsigned char *x);
+void print_matrix(int *p, int h, int w);
+void my_strncpy(char *dest, char *src, int n);
+void er_hand(const char *msg);
+void write_in_file(int fd, int id, const char *msg);
+void work_with_log(int fd, int sem_id, struct sembuf my_sem_buf, int client_id, const char *msg);
+
+int main()
+{
+	const int one=sizeof(int);
+	int client_id, client_num;
+
+	key_t key_sem=ftok(FILE_IN, IPC_ID_SEM);	
+	key_t key_msg=ftok(FILE_IN, IPC_ID_MSG);
+	key_t key_shm=ftok(FILE_IN, IPC_ID_SHM);
+	if ((key_sem<0)||(key_msg<0)||(key_shm<0)) er_hand("Can't generate a key");
+
+
+	(void)umask(0);
+	int er_sem, er_rd, fd;
+	char *cl_id;
+	int *id;
+	
+	struct sembuf my_sem_buf;
+	my_sem_buf.sem_num=0;
+	my_sem_buf.sem_flg=0;
+
+	int sem_id=semget(key_sem, 1, 0666|IPC_CREAT|IPC_EXCL);
+	if ((sem_id<0)&&(errno!=EEXIST))
+	{
+		er_hand("Can't create semapfore");
+	}
+	
+	(void)umask(0);
+	int shm_id=shmget(key_shm, one, 0666|IPC_CREAT);
+	if (shm_id<0) er_hand("Can't create shared memory");
+	
+	cl_id=shmat(shm_id, NULL, 0);
+	if (cl_id==(char*)(-1)) er_hand("Can't attach shared memory");
+	id=(int*)cl_id;
+	
+	if (sem_id>0) //the first client
+	{
+		// semaphore has been created, value=0 (man)
+		//critical section
+		client_num=client_id=1;
+		(*id)=5;
+
+		(void)umask(0);
+		fd=open(FILE_LOG, O_WRONLY|O_CREAT|O_APPEND|O_TRUNC,0666);
+		if (fd<0) er_hand("Can't open file");
+
+		write_in_file(fd, client_id, "I have begun working and created a semaphore\n");			
+		//end of critical section
+		my_sem_buf.sem_op=1;
+		er_sem=semop(sem_id, &my_sem_buf, 1);
+		if (er_sem<0) er_hand("Can't operate with semaphore 1");
+	}
+	if ((sem_id<0)&&(errno==EEXIST)) //not the first client
+	{
+		sem_id=semget(key_sem, 1, 0666);
+		if (sem_id<0) er_hand("Can't get access to semaphore");
+
+		my_sem_buf.sem_op=-1;
+		er_sem=semop(sem_id, &my_sem_buf, 1);
+		if (er_sem<0) er_hand("Can't operate with semaphore 2a");
+
+		//critical section
+		client_id=*id;
+		(*id)+=4;
+		client_num=1+client_id/4;
+
+		(void)umask(0);
+		fd=open(FILE_LOG, O_WRONLY|O_APPEND);
+		if (fd<0) er_hand("Can't open file");
+
+		write_in_file(fd, client_num, "I have begun working\n");
+		//end of critical section
+		my_sem_buf.sem_op=1;
+		er_sem=semop(sem_id, &my_sem_buf, 1);		
+		if (er_sem<0) er_hand("Can't operate with semaphore 2b");
+	}
+
+	er_rd=shmdt(cl_id);
+	if (er_rd<0) printf("Can't detach shared memory\n errno=%d\n",errno);
+
+	(void)umask(0);
+	int msg_id=msgget(key_msg, 0666|IPC_CREAT);
+	if (msg_id<0) er_hand("Can't create message queue");
+
+	struct msgbuf_str buffer[1];
+	struct msgbuf_matr buf2[1];
+
+	char hello[]="hello";
+	strcpy(buffer[0].msg, hello);
+	buffer[0].mtype=client_id;
+
+	er_rd=msgsnd(msg_id, &buffer, strlen(hello)+1,0);
+	if (er_rd<0) er_hand("Can't send a message");
+
+	work_with_log(fd, sem_id, my_sem_buf, client_num, "I have sent hello message to server\n");
+	
+	er_rd = msgrcv(msg_id, &buf2, MSG_MAX, client_id+1, 0);
+	if (er_rd<0) er_hand("Can't receive message from server");
+	
+	work_with_log(fd, sem_id, my_sem_buf, client_num, "I have received work from server\n");
+
+	int h1=buf2[0].info.height1;
+	int h2=buf2[0].info.height2;
+	int w=buf2[0].info.width;
+	int *m;
+	m=buf2[0].info.matrices;	
+
+	// MULTIPLICATION
+
+	struct mult job;
+	job.p1 = m;
+	job.p2 = m + h1*w;
+	job.wh = w;
+	int *result;
+	result=(int*)malloc(h1*h2*one);
+	
+	int i,j;
+	for (i=0; i<h1; i++)
+	{	
+		job.str1=i;
+		for (j=0; j<h2; j++)
+		{
+			job.res = result+i*h2+j;
+			job.stlb2 = j;
+			multiplication(&job);
+		}
+	}
+
+	work_with_log(fd, sem_id, my_sem_buf, client_num, "Hurray! I have done my work!\n");
+
+	my_strncpy(buffer[0].msg,(char*)(result), h1*h2*one);
+	buffer[0].mtype=client_id+2;
+
+	er_rd=msgsnd(msg_id, &buffer, h1*h2*one, 0);
+	if (er_rd<0) er_hand("Can't send a message with work");
+
+	free(result);
+
+	work_with_log(fd, sem_id, my_sem_buf, client_num, "I have sent my work to server\n");
+
+	er_rd=msgrcv(msg_id, &buffer, MSG_MAX, client_id+3, 0);
+	if (er_rd<0) printf("Can't receive last message from server\n");
+
+	if (strstr(buffer[0].msg,"last client")>0)
+	{
+		if ((semctl(sem_id,0,IPC_RMID,NULL)<0)||
+		(shmctl(shm_id,IPC_RMID, NULL)<0)||
+		(msgctl(msg_id,IPC_RMID,NULL)<0)) er_hand("Can't remove IPC");
+		else printf("All IPCs have been deleted successfully\n");	
+	}
+	else
+	{
+		buffer[0].mtype=client_id;
+		er_rd=msgsnd(msg_id, &buffer, 0, 0);
+		if (er_rd<0) er_hand("Can't send to server");
+	}
+	printf("Client %d finished it's work successfully\n",client_num);
+	return 0;
+}
+
+void er_hand(const char *msg)
+{
+	printf("%s\n",msg);
+	printf("errno=%d\n",errno);
+	exit(-1);
+};
+
+void write_in_file(int fd, int id, const char *msg)
+{
+	int er;
+	int pos=0;
+	int size,k;
+	
+	int i=id;
+	int j=0;
+	char *x;
+	while (i>0)
+	{
+		i/=10;
+		j++;
+	}
+	k=j;
+	size=j+strlen(msg)+1;
+	x=(char*)malloc(size);
+	i=id;
+	x[j]=' ';
+	j--;
+	while (i>0)
+	{
+		x[j]=(i%10)+48;
+		i/=10;
+		j--;
+	}
+	for (i=k+1; i<size; i++) x[i]=msg[i-k-1];		
+	do 
+	{
+		er=write(fd,x+pos,size-pos);
+		pos+=er;		
+	} while (er>0);
+	if (er<0) er_hand("Can't write in file");
+	free(x);
+};
+
+void work_with_log(int fd, int sem_id, struct sembuf my_sem_buf, int client_id, const char *msg)
+{
+	int er_sem;
+
+	my_sem_buf.sem_op=-1;
+	er_sem=semop(sem_id, &my_sem_buf, 1);
+	if (er_sem<0)
+	{
+		printf("%d\n",client_id);
+//		er_hand("Can't operate with semaphore");
+		er_hand(msg);
+	}
+
+	write_in_file(fd, client_id, msg);
+
+	my_sem_buf.sem_op=1;
+	er_sem=semop(sem_id, &my_sem_buf, 1);		
+	if (er_sem<0)// er_hand("Can't operate with semaphore");
+		er_hand(msg);
+};
+
+void my_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	for (i=0; i<n; i++) *(dest+i)=*(src+i);
+};
+
+void *multiplication(void *num0)
+{
+	int i;
+	int res=0;
+	int one=sizeof(int);
+	int *str, *stlb;
+	struct mult *num;
+	num=(struct mult *)num0;
+	str = (*num).p1 + (*num).wh * (*num).str1;
+	stlb = (*num).p2 + (*num).wh * (*num).stlb2;
+
+	for (i=0; i<(*num).wh; i++)
+	{
+		res += *(str+i) * *(stlb+i);
+	} 
+	*(num[0].res)=res;
+	
+	return NULL;
+};
+
+void print_matrix(int *p, int h, int w)
+{
+	int i,j,k;
+	int one=sizeof(int);
+	int buf;
+	for (i=0; i<h; i++)
+	{
+		for (j=0; j<w; j++)
+		{
+			buf=*(p+i*w+j);			
+			printf("%d ", buf);
+		}
+		printf("\n");
+	}
+	printf("\n");
+};
+
+int char_to_int(unsigned char *x)
+{
+	int one=sizeof(int);
+	int buf=0;
+	int log2=1;
+	int k;
+	for (k=0; k<one; k++)
+	{
+		buf+=(*(x+k))*log2;
+		log2*=256;
+	}
+	return buf;
+};
+
+
---
+++ server.c
@@ -0,0 +1,401 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <errno.h>
+#include <pthread.h>
+
+#define FILE_OUT "./result.txt"
+#define FILE_IN "./source.txt"
+#define IPC_ID_MSG 1
+#define MSG_MAX 4068
+#define HELLO_MSG "hello"
+#define BYE_MSG "bye!"
+#define LAST_BYE_MSG "bye, last client!"
+
+struct mult
+{
+	int str1;
+	int stlb2;
+	int wh;        		 //width (height for second matrix)
+	int *p1, *p2;  	 //pointers to the matrices
+	int *res;      //result of calculation
+};
+
+struct thread_arg
+{
+	int i; //determines client's ID
+	int msg_q_id;
+	int w;
+	int h1;
+	int h2;
+	int n,user;
+	int *res,*m1,*m2;
+};
+
+struct msgbuf_matr
+{
+	long mtype;
+	struct
+	{
+		int width;
+		int height1;
+		int height2;
+		int matrices[MSG_MAX];
+	} info;
+};
+
+struct msgbuf_str
+{
+	long mtype;
+	char msg[MSG_MAX];
+};
+
+void *multiplication(void *num0);
+void print_matrix(int *p, int h, int w); 
+void receive_matrix(int *m0, int fd, int h, int w);
+void my_strncpy(char *dest, char *src, int n);
+void err_hand(const char *msg);
+void *routine(void *my_th_arg);
+void matrix_in_file(int fd, int *m, int h, int w);
+
+int main(int argc, char **argv)
+{
+	// Receiving the information about matrices
+	
+	int fd=open(FILE_IN, O_RDONLY);
+	if (fd<0) err_hand("Can't open file for reading");
+		
+	int a[3];
+	int i,j,er_rd;
+	const int one=sizeof(int);
+
+	//receiving the parameters of matrices
+	for (i=0; i<3; i++) receive_matrix(a+i,fd,1,1);
+	
+	const int h1=a[0]; 
+	const int h2=a[2];
+	const int w=a[1];
+	int s1=h1*w;
+	int s2=h2*w;
+	int *m1,*m2;
+
+	m1=(int*)malloc(s1*one);
+	m2=(int*)malloc(s2*one);
+
+	receive_matrix(m1,fd,h1,w);
+	receive_matrix(m2,fd,h2,w);
+
+	er_rd=close(fd);
+	if (er_rd<0) printf("Can't close file\n");
+	
+	// MULTIPLICATION
+
+	// Creating message queues
+
+	key_t key=ftok(FILE_IN, IPC_ID_MSG);
+	if (key<0) err_hand("Can't generate a key for message queue");
+	int msg_q_id=msgget(key,0666|IPC_CREAT);
+	if (msg_q_id<0) err_hand("Can't create message queue");
+
+	const int n=atoi(argv[1]);    //number of clients
+	int user, much_work, max_work;
+	int last_user=0;
+	if (n>=h1)
+	{
+		much_work=0;
+		user=1;	
+		max_work=w*(h2+1)*one;
+	}
+	else
+	{
+		much_work=1;
+		user=h1/n; //number of lines in first matrix for usual (not last) client 
+		last_user=user+h1%n;
+		max_work = (last_user+h2)*one; //number of bytes in greatest message
+	}
+
+	if (max_work>MSG_MAX) err_hand("Too large matrices");
+
+	const int size_res=h1*h2;
+
+	int *result2;
+	result2=(int*)malloc(size_res*one);
+
+	struct thread_arg my_th_arg[n];
+	pthread_t my_threads[n];
+
+	for (i=0; i<n; i++)
+	{
+		printf("Creating thread for client %d...\n",i+1);
+		my_th_arg[i].msg_q_id=msg_q_id;
+		my_th_arg[i].i=i;
+		my_th_arg[i].m1=m1;
+		my_th_arg[i].m2=m2;
+		my_th_arg[i].w=w;
+		my_th_arg[i].h2=h2;
+		my_th_arg[i].n=n;
+		my_th_arg[i].user=user;
+
+		if ((much_work==1)||(i<h1)) my_th_arg[i].res=result2+i*user*h2;
+		else my_th_arg[i].res=result2;
+
+		if ((much_work==1)&&(i==(n-1))) my_th_arg[i].h1=last_user;
+		if ((much_work)&&(i!=(n-1))) my_th_arg[i].h1=user;
+		if ((much_work==0)&&(i<h1)) my_th_arg[i].h1=1;
+		if ((much_work==0)&&(i>=h1)) my_th_arg[i].h1=0;
+
+		er_rd=pthread_create(my_threads+i, NULL, routine, my_th_arg+i);
+		if (er_rd!=0) err_hand("Can't create a thread");	
+	}
+
+	for (i=0; i<n; i++)
+	{
+		er_rd=pthread_join(my_threads[i],NULL);
+		if (er_rd!=0) err_hand("Can't join a thread\n");
+		printf("Thread of client %d was joined successfully\n",i+1);
+	}
+	
+	struct msgbuf_str buffer[1];
+	buffer[0].mtype=4*n;
+	strcpy(buffer[0].msg, LAST_BYE_MSG);
+	int sz=strlen(LAST_BYE_MSG);
+	er_rd = msgsnd(msg_q_id, &buffer, sz+1, 0);
+	if (er_rd<0) err_hand("Can't send bye to last client");	
+
+	free(m1);
+	free(m2);
+
+	(void)umask(0);
+	fd=open(FILE_OUT, O_WRONLY|O_CREAT|O_TRUNC);
+	if (fd<0) err_hand("Can't open file");
+	matrix_in_file(fd,result2, h1, h2);
+	er_rd=close(fd);
+	if (er_rd<0) err_hand("Can't close file");
+
+	free(result2);
+
+	printf("Goodbye!!!\n");
+
+	return 0;
+}
+
+void *routine(void *my_th_arg)
+{
+	struct thread_arg *x;
+	x=(struct thread_arg*)my_th_arg;
+
+	int i=x[0].i;
+	int msg_q_id=x[0].msg_q_id;
+	int w=x[0].w;
+	int h1=x[0].h1;
+	int h2=x[0].h2;
+	int n=x[0].n;
+	int user=x[0].user;
+	int *res,*m1,*m2;
+	res=x[0].res;
+	m1=x[0].m1;
+	m2=x[0].m2;	
+
+	struct msgbuf_str buffer[1];
+	struct msgbuf_matr buf2[1];
+	int one=sizeof(int);
+	int sz;
+	int s2=h2*w*one;
+	int work=h1*w*one+s2;
+	if (h1==0) work=0;
+
+	int er_rd=msgrcv(msg_q_id, &buffer, MSG_MAX, 4*i+1, 0);
+	if (er_rd<0)
+	{
+		printf("Can't receive message from client No %d\n",i);
+		exit(-1);
+	}
+
+	buf2[0].mtype=4*i+2;
+	buf2[0].info.width = w;
+	buf2[0].info.height1 = h1;
+	buf2[0].info.height2 = h2;
+	if (work>0)
+	{
+		my_strncpy((char*)buf2[0].info.matrices, (char*)(m1 + user*i*w), h1*w*one);
+		my_strncpy((char*)(buf2[0].info.matrices + h1*w), (char*)m2, s2);
+	}
+
+	er_rd = msgsnd(msg_q_id, &buf2, work+3*one, 0);
+	if (er_rd<0)
+	{
+		printf("Can't send message to client No %d",i+1);
+		exit(-1);
+	}
+		
+	er_rd=msgrcv(msg_q_id, &buffer, MSG_MAX, 4*i+3, 0);
+	if (er_rd<0) err_hand("Can't receive done work from client");
+
+	my_strncpy((char*)(res), buffer[0].msg, h1*h2*one);
+	buffer[0].mtype=4*i+4;
+	if (i!=n-1)
+	{
+		strcpy(buffer[0].msg, BYE_MSG);
+		sz=strlen(BYE_MSG);
+		er_rd = msgsnd(msg_q_id, &buffer, sz+1, 0);
+		if (er_rd<0) printf("Can't send last message to client, errno=%d\n",errno);
+		er_rd = msgrcv(msg_q_id, &buffer, MSG_MAX, 4*i+1, 0);
+		if (er_rd<0) err_hand("Can't receive last message");
+	}
+	
+	return NULL;
+}
+
+void err_hand(const char *msg)
+{
+	printf("%s\n",msg);
+	printf("errno=%d\n",errno);
+	exit(-1);
+}
+
+void my_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	for (i=0; i<n; i++) *(dest+i)=*(src+i);
+}
+
+void receive_matrix(int *m0, int fd, int h, int w)
+{
+	int position=0;
+	int ind_err=1;
+	int one=sizeof(int);
+	int size=h*w*one;
+	char *m;
+	m=(char*)m0;
+	
+	while (ind_err>0)
+	{
+		ind_err=read(fd, m+position, size-position);
+		position+=ind_err;
+	}
+	if (ind_err<0) err_hand("Can't read from file");
+};
+
+
+void *multiplication(void *num0)
+{
+	int i;
+	int res=0;
+	int one=sizeof(int);
+	int *str, *stlb;
+	struct mult *num;
+	num=(struct mult *)num0;
+	str = (*num).p1 + (*num).wh * (*num).str1;
+	stlb = (*num).p2 + (*num).wh * (*num).stlb2;
+	
+	for (i=0; i<(*num).wh; i++)
+	{
+		res += *(str+i) * *(stlb+i);
+	} 
+	*(num[0].res)=res;
+	return NULL;
+};
+
+void print_matrix(int *p, int h, int w)
+{
+	int i,j,k;
+	int one=sizeof(int);
+	int buf;
+	for (i=0; i<h; i++)
+	{
+		for (j=0; j<w; j++)
+		{
+			buf=*((int*)(p+i*w+j));			
+			printf("%d ", buf);
+		}
+		printf("\n");
+	}
+	printf("\n");
+};
+
+void matrix_in_file(int fd, int *m, int h, int w)
+{
+	int sz=h*w*2+h; ///assume that all values are numbers
+	int new_sz;
+	int x,signum;
+	char *res,*buf;
+	res=(char*)malloc(sz);
+	int i,j,k,bufer,razr;
+	int pos=0;
+	for (i=0; i<h; i++)
+	{
+		for (j=0; j<w; j++)
+		{
+
+			x=*(m+i*w+j);
+			if (x<0)
+			{
+				x=-x;
+				bufer=x;
+				signum=1;
+			}
+			else
+			{
+				bufer=x;
+				signum=0;
+			}
+
+			k=0;
+			while(bufer>0)
+			{
+				k++;
+				bufer/=10;
+			}				
+			bufer=x;	
+			razr=k+signum; 
+			buf=malloc(razr);
+			if (signum) buf[0]='-';
+
+			for (k=razr-1; k>=signum; k--)
+			{
+				buf[k]=48+bufer%10;
+				bufer/=10;
+			}
+
+			new_sz=razr+pos+1;
+			if (new_sz>sz)
+			{
+				res=realloc((char*)res, new_sz);
+				sz=new_sz;
+			}
+			k=0;
+			while (pos<new_sz-1)
+			{
+				*(res+pos)=*(buf+k);
+				k++;
+				pos++;
+			}
+			free(buf);
+			*(res+pos)=' ';
+			pos++;
+		}
+		if (pos+1>sz)
+		{
+			res=realloc((char*)res, pos+1);
+			sz++;
+		}
+		*(res+pos)='\n';
+		pos++;
+	}
+	pos=0;
+	int g;
+	do
+	{
+		g=write(fd,res+pos,sz-pos);
+		pos+=g;
+	} while (g>0);
+	if (g<0) err_hand("Can't write in file");
+	free(res);	
+};
